693d73cfd7096468fa90d43456ddf41cf904954f
franktinongbe <franktinongbe86@gmail.com>
2025-07-06 23:46:53 +0200

    update last

---
+++ Backend/middlewares/authorize.js
@@ -0,0 +1,12 @@
+// middlewares/authorize.js
+
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Accès interdit. Rôle insuffisant.' });
+    }
+    next();
+  };
+};
+
+module.exports = authorize;
